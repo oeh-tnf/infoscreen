b45fd72c71bbfdd5e2637294bcf9319688520293
jstsmthrgk <jstsmthrgk@jstsmthrgk.eu>
2025-10-20 18:08:29 +0200

    ooops

--- app/DeparturesSortFilterProxyModel.cpp
+++ app/DeparturesSortFilterProxyModel.cpp
@@ -14,7 +14,6 @@ void DeparturesSortFilterProxyModel::setTime(QDateTime time) {
 
 bool DeparturesSortFilterProxyModel::filterAcceptsRow(int source_row, const QModelIndex &source_parent) const
 {
-	return true;
 	return sourceModel()->data(sourceModel()->index(source_row, 0, source_parent), DepartureModel::TimeRole).toDateTime() >= m_time;
 }
 
